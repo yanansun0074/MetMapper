7b9a497d93e542f1c86586dd6ae88906de1ad37b
Kevin W <kwang10082000@gmail.com>
2023-05-08 12:34:48 -0400

    locomotion changes and arms_armor info added

---
+++ metamappers_combined/Assets/Scripts/scene1_scripts/halberd.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class halberd : Information
+{
+    void Awake()
+    {
+        artName = "Halberd";
+        artistYear = "Europe, 13th - 16th century";
+        description = "A halberd (also called halbard, halbert or Swiss voulge) is a two-handed pole weapon that came to prominent use during the 13th, 14th, 15th, and 16th centuries. The halberd consists of an axe blade topped with a spike mounted on a long shaft. It can have a hook or thorn on the back side of the axe blade for grappling mounted combatants. The halberd was usually 1.5 to 1.8 metres (5 to 6 feet) long.";
+    }
+}
---
+++ metamappers_combined/Assets/Scripts/scene1_scripts/shield.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shield : Information
+{
+    void Awake()
+    {
+        artName = "Shield with Lion";
+        artistYear = "Europe, 1300s";
+        description = "Medieval shield slant, the center is depicted a lion rampant. The lions have always been very popular in the heraldry of European Royalty";
+    }
+}
