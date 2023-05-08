ca4c5f7d38d1c932fc44dccc4c4a211a7326a621
yanansun0074 <ys3610@columbia.edu>
2023-05-08 03:57:47 -0400

    Wayfinding (arrow) improved + Chinese scene cleanup

--- metamappers_combined/Assets/Scripts/ObjectFinding.cs
+++ metamappers_combined/Assets/Scripts/ObjectFinding.cs
@@ -15,53 +15,24 @@ public class ObjectFinding : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // targets = new List<GameObject>();
+        
         ObjFinder = GameObject.Find("TargetManager").GetComponent<TargetsManager>();
-        // targets = ObjFinder.targets;
+        
 
     }
 
     public void AddTarget()
     {
         string obj_name = this.GetComponentInChildren<TextMeshProUGUI>().text;
-        // if(targets[0] != null)
-        // {
-        //     // End the highlight
-        //     removeHighlight(targets[0]);
-        //     ObjFinder.RemoveTarget();
-        //     targets.RemoveAt(0);
-        // }
-        // this.GetComponentInChildren<TextMeshProUGUI>().text = "success!";
+    
         GameObject target = GameObject.Find(obj_name);
-        // target.SetActive(false);
-        // if(targets.IndexOf(target) == -1)
-        // {
-        // targets.Add(target);
+        
         ObjFinder.AddTarget(target);
-        // }
+        
         
 
-        ObjFinder.HighlightTarget();
         
     }
 
-    // public void highlightTarget(GameObject target)
-    // {
-    //     if(target != null)
-    //     {
-    //         Renderer r = target.GetComponent(typeof(Renderer)) as Renderer;
-    //         oldColor = r.material.GetColor("_Color");
-    //         Color newColor = new Color(oldColor.r + 0.2f, oldColor.g + 0.2f, oldColor.b + 0.2f, oldColor.a);
-    //         r.material.SetColor("_Color", newColor);
-    //     }
-    // }
-
-    // public void removeHighlight(GameObject old_obj)
-    // {
-    //     if(old_obj != null)
-    //     {
-    //         Renderer r = old_obj.GetComponent(typeof(Renderer)) as Renderer;
-    //         r.material.SetColor("_Color", oldColor);
-    //     }
-    // }
+    
 }
